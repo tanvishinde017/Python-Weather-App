a5cfe50914283d58db37b25808cda4d3f375c10c
tanvi shinde <tanvishinde1560@gmail.com>
2026-02-05 10:31:23 +0000

    port changed

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 80
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def index():
     return render_template("index.html", weather=weather, error=error)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
 
